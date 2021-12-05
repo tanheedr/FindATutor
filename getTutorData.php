6b120b9667f252596869d6b346f3dc89dc5fe839
tanheedr <93531903+tanheedr@users.noreply.github.com>
2021-12-05 19:52:25 +0000

    Add files via upload

--- getTutorData.php
+++ getTutorData.php
@@ -10,26 +10,28 @@ if (isset($_GET['subject'])){
     while($row = mysqli_fetch_assoc($result)){
         array_push($response,
         array(
-            'TutorID' = $row['TutorID'],
-            'Subjects' = $row['Subjects'],
-            'HourlyCost' = $row['HourlyCost'],
-            'Qualifications' = $row['Qualifications'],
-            'Description' = $row['Description'])
+            'FirstName' => $row['FirstName'],
+            'Surname' => $row['Surname'],
+            'Subjects' => $row['Subjects'],
+            'HourlyCost' => $row['HourlyCost'],
+            'Qualifications' => $row['Qualifications'],
+            'Description' => $row['Description'])
         );
     }
     echo json_encode($response);
 }else{
-    $query = "SELECT * FROM tutordescription";
+    $query = "SELECT FirstName, Surname, Subjects, HourlyCost, Qualifications, Description FROM accounts, tutordescription WHERE TutorID = ID";
     $result = mysqli_query($connection, $query);
-    $response = array()
+    $response = array();
     while($row = mysqli_fetch_assoc($result)){
         array_push($response,
         array(
-            'TutorID' = $row['TutorID'],
-            'Subjects' = $row['Subjects'],
-            'HourlyCost' = $row['HourlyCost'],
-            'Qualifications' = $row['Qualifications'],
-            'Description' = $row['Description'])
+            'FirstName' => $row['FirstName'],
+            'Surname' => $row['Surname'],
+            'Subjects' => $row['Subjects'],
+            'HourlyCost' => $row['HourlyCost'],
+            'Qualifications' => $row['Qualifications'],
+            'Description' => $row['Description'])
         );
     }
     echo json_encode($response);
